89926e4c10430baee2868c1dc358bc9ec09cf079
Dorian Sousa <calvo.dorian14@gmail.com>
2023-11-29 22:19:23 +0100

    made opencv work on simple graphs

--- algo-OpenCV.py
+++ algo-OpenCV.py
@@ -2,19 +2,19 @@ import cv2
 import numpy as np 
 
 #image_path = "newGood/randomGraph50_80.png"
-folder_path = "newGood/"
-image_path = "randomGraph50_80.png"
+folder_path = "mySimpleGraphs/"
+image_path = "very simple graph4.png"
 
 #load image
 image = cv2.imread(folder_path + image_path)
 
-def drawCircles(detected_circles, image):
+def drawCircles(circles, image):
 
   #Convert the circle parameters a, b and r to integers. 
-  detected_circles = np.uint16(np.around(detected_circles)) 
+  circles = np.uint16(np.around(circles)) 
   
-  for pt in detected_circles[0, :]: 
-    a, b, r = pt[0], pt[1], pt[2] 
+  for circle in circles: 
+    a, b, r = circle 
     #Draw the circumference of the circle. 
     cv2.circle(image, (a, b), r, (0, 255, 0), 2) 
 
@@ -23,12 +23,22 @@ def drawCircles(detected_circles, image):
   
   cv2.imwrite("detected circles.png",image)
 
+def drawLines(lines_list, image):
+  for l in lines_list:
+    x1,y1,x2,y2=l
+    color = tuple(np.random.randint(0, 256, size=3))
+    color = ( int (color [ 0 ]), int (color [ 1 ]), int (color [ 2 ])) 
+    cv2.line(image, (x1,y1), (x2,y2), color, 2)
+
+  cv2.imwrite('detected Lines.png', image)
+
 
 def drawEdges(lines_list, image):
-  for p1, p2 in lines_list:
-    cv2.line(image, p1, p2, (0,255,0), 2)
+  for l in lines_list:
+    x1,y1,x2,y2=l
+    cv2.line(image, (x1,y1), (x2,y2), (0,255,0), 2)
 
-  cv2.imwrite('detected lines.png', image)
+  cv2.imwrite('detected Edges.png', image)
 
 
 def detectCircles(image): #https://www.geeksforgeeks.org/circle-detection-using-opencv-python/
@@ -36,19 +46,26 @@ def detectCircles(image): #https://www.geeksforgeeks.org/circle-detection-using-
   kernel = np.ones((5, 5), np.uint8)
 
   #erode and then dilate image to remove edges (For some reason the functions to the opposite of what they should do)
-  image_circles = cv2.dilate(image, kernel, iterations = 1)  
-  image_circles = cv2.erode(image_circles, kernel, iterations = 1)  
+  image_circles = cv2.dilate(image, kernel, iterations = 5)  
+  image_circles = cv2.erode(image_circles, kernel, iterations = 5)  
   #Convert to grayscale. 
   gray = cv2.cvtColor(image_circles, cv2.COLOR_BGR2GRAY) 
 
   detected_circles = cv2.HoughCircles(gray,  
-                  cv2.HOUGH_GRADIENT, 1, 25, param1 = 30, 
-              param2 = 13.49, minRadius = 8, maxRadius = 40) 
+                  cv2.HOUGH_GRADIENT, 1, 100, param1 = 15, 
+              param2 = 15, minRadius = 114, maxRadius = 125) 
   
   cv2.imwrite("Circles.png", image_circles)
-  drawCircles(detected_circles, image_circles)
 
-  return detected_circles
+  circles = []
+  for pt in detected_circles[0, :]: 
+    a, b, r = pt[0], pt[1], pt[2] 
+
+    circles.append((a,b,r))
+
+  drawCircles(circles, image_circles)
+
+  return circles
 
 def detectEdges(img): #https://www.geeksforgeeks.org/line-detection-python-opencv-houghline-method/
 
@@ -66,11 +83,11 @@ def detectEdges(img): #https://www.geeksforgeeks.org/line-detection-python-openc
   lines_list =[]
   lines = cv2.HoughLinesP(
               edges, #Input edge image
-              1, #Distance resolution in pixels
+              2, #Distance resolution in pixels
               np.pi/180, #Angle resolution in radians
-              threshold=40, #Min number of votes for valid line
-              minLineLength=30, #Min allowed length of line
-              maxLineGap=10 #Max allowed gap between line for joining them
+              threshold=55, #Min number of votes for valid line
+              minLineLength=50, #Min allowed length of line
+              maxLineGap=70 #Max allowed gap between line for joining them
               )
   
   #Iterate over points
@@ -79,13 +96,87 @@ def detectEdges(img): #https://www.geeksforgeeks.org/line-detection-python-openc
     x1,y1,x2,y2=points[0]
 
     #Maintain a simples lookup list for points
-    lines_list.append([(x1,y1),(x2,y2)])
+    lines_list.append([x1,y1,x2,y2])
   
-  drawEdges(lines_list, img)
-  return lines_list
+  drawLines(lines_list, img.copy())
 
+  edges = mergeLinesToEdge(lines_list)
+
+  drawEdges(edges,img.copy())
+
+  return edges
+
+def mergeLinesToEdge(lines_list):
+  edges = []
+  for l1 in lines_list:
+    lines_list.remove(l1)
+    x11,y11,x12,y12=l1
+    for l2 in lines_list:
+      x21,y21,x22,y22=l2
+
+      if abs(x11-x21)<10 and abs(y11-y21)<10 and abs(x12-x22)<10 and abs(y12-y22)<10: #dont know if there is guarantee that x11 and x21 are closest together and not x11 and x22
+        lines_list.remove(l2)
+        x1 = int((x11+x21)/2)
+        y1 = int((y11+y21)/2)
+        x2 = int((x12+x22)/2)
+        y2 = int((y12+y22)/2)
+
+        edges.append([x1,y1,x2,y2])
+
+  return edges
+
+def checkIfInCircle(point,circles):
+  for circle in circles:
+    cx, cy, r = circle[0], circle[1], circle[2]
+    px, py = point
+    threshold=10
+    if px >= cx-r-threshold and px <= cx+r+threshold and py >= cy-r-threshold and py <= cy+r+threshold:
+      return circle
+  
+  return False
+
+def connectVertices(vertices, edges):
+  adj_list = {}
+  for v in vertices:
+  	adj_list[v] = set()
+
+  for edge in edges:
+    v1 = checkIfInCircle((edge[0], edge[1]), vertices)
+    v2 = checkIfInCircle((edge[2], edge[3]), vertices)
+
+    adj_list[v1].add(v2)
+    adj_list[v2].add(v1)
+  
+  return adj_list
+
+def drawExtractedGraph(vertices, edges):
+  blank_image = np.zeros((1000,1000,3), np.uint8)
+
+  #Convert the circle parameters a, b and r to integers. 
+  vertices = np.uint16(np.around(vertices)) 
+  
+  for v in vertices: 
+    a, b, r = v 
+    #Draw the circumference of the circle. 
+    cv2.circle(blank_image, (a, b), r, (0, 255, 0), 2) 
+
+    #Draw a small circle (of radius 1) to show the center. 
+    cv2.circle(blank_image, (a, b), 1, (0, 0, 255), 3) 
+
+  for edge in edges:
+    x1,y1,x2,y2 = edge
+    cv2.line(blank_image, (x1,y1), (x2,y2), (0,0,255), 2)
+
+  cv2.imwrite("Extracted Graph.png", blank_image)
 
 detected_circles = detectCircles(image)
 edges = detectEdges(image)
+
+adj_list = connectVertices(detected_circles, edges)
+
+print(adj_list)
+
+drawExtractedGraph(detected_circles, edges)
+
 
   
